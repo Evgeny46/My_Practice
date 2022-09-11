c2380f184dbd6b162e998af45988abf5dc3e2ff6
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-11 21:40:01 +0300

    Создали конвертер валют

---
+++ Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+double USDtoRUB = 60.47;
+double USDtoCNY = 6.92;
+double USD;
+
+Console.Write("Введите сумму в USD: ");
+
+USD = double.Parse(Console.ReadLine());
+
+Console.WriteLine(USD + " USD в RUB = " + USD * USDtoRUB);
+Console.WriteLine(USD + " USD в CNY = " + USD * USDtoCNY);
+
