1efcf88aedc9e6f24462ad91bf2f35688620364c
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-11 17:33:08 +0300

    Добавили пример конвертации

---
+++ Convert/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+string str;
+int a, b;
+
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+
+Console.Write("Введите следующее число: ");
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+
+int result = a + b;
+
+Console.Write($"Сумма чисел = {result}");
+
+
+
+// Console.Write("Введите первое число: ");
+// string a = Console.ReadLine();
+// int b = int.Parse(a);
+
+// Console.Write("Введите второе число: ");
+// string c = Console.ReadLine();
+// int d = int.Parse(c);
+
+// int e = b + d;
+
+// Console.WriteLine("Сумма чисел = " + e);
+
+
+
+
+
+
+
+
