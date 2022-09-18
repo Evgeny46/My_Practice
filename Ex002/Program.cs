5ea1274191d8cc5dca998c2779b9b59ea69e71af
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-18 16:23:55 +0300

    Написали программу калькулятор 2 способами

---
+++ Ex002/Program.cs
@@ -0,0 +1,85 @@
+﻿// Написать программу, которая выполняет функцию калькулятора
+
+/* Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите операцию из списка: +, -, *, /");
+
+string c = Console.ReadLine();
+
+if(c == "+")
+{
+    double sum = a + b;
+    Console.WriteLine(sum);
+}
+
+
+if(c == "-")
+{
+    double sub = a - b;
+    Console.WriteLine("Ответ: " + sub);
+
+}
+
+if(c == "*")
+{
+    double mult = a * b;
+    Console.WriteLine("Ответ: " + mult);
+
+}
+
+if(c == "/")
+{
+    double div = a / b;
+    Console.WriteLine("Ответ: " + div);
+
+
+*/
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите операцию из списка: +, -, *, /");
+string operation = Console.ReadLine(); 
+
+switch (operation)
+{
+    case "+":
+            double sum = a + b;
+            Console.WriteLine("Ответ: " + sum);  
+        break;
+
+    case "-":
+            double sub = a - b;
+            Console.WriteLine("Ответ: " + sub);
+        break;
+
+    case "*":
+            double mult = a * b;
+            Console.WriteLine("Ответ: " + mult);
+        break;
+
+    case "/":
+            if(b == 0)
+                Console.WriteLine(0);
+            else
+            {
+            double div = a / b;
+            Console.WriteLine("Ответ: " + div);
+            }
+        break;
+    
+    default:
+            Console.WriteLine("Ошибка! Неизвестное действие");
+            break;
+} 
+
+
+
