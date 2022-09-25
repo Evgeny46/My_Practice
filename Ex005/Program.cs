ccff4d719dddfebeb08a5c3112facf354fdfc37c
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-25 21:51:46 +0300

    Вложенные циклы

---
+++ Ex005/Program.cs
@@ -0,0 +1,10 @@
+﻿//Вложенные циклы
+for(int i = 1; i <= 3; i++)
+{
+    Console.WriteLine("Цикл 1, итерация № " + i);
+
+    for(int j = 1; j<=3; j++)
+    {
+        Console.WriteLine("\tЦикл 2, итерация № " + j);
+    }
+}
