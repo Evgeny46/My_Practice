6bc7e7087fd6e28886e1d8806cf08255252cfdcc
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-25 22:03:37 +0300

    Вывод фигуры в консоли

---
+++ Ex006/Program.cs
@@ -0,0 +1,17 @@
+﻿//Нарисовать квадрат
+
+Console.WriteLine("Введите высоту фигуры: ");
+int height = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите ширину фигуры: ");
+int width = int.Parse(Console.ReadLine());
+
+for (int i = 0; i<=height; i++)
+{
+    for(int j = 0; j<=width; j++)
+    {
+        Console.Write("* ");
+    }
+
+    Console.WriteLine();
+}
