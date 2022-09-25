3d3293cca49822bf24fca5729d21efe7a0a80daf
Evgeniy <bridskijevgeny@yandex.ru>
2022-09-25 12:59:58 +0300

    Операции инкремента и декремента в цикле

---
+++ Ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Используя цикл for выведите в 2 переменные, которые будут выводить значения в обратном порядке и от меньшего к большему
+
+Console.WriteLine("Введите значение первой переменной: ");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение второй переменной");
+int j = int.Parse(Console.ReadLine());
+
+for (; i <= j && j>=i; i++, j--)
+{
+    Console.WriteLine("переменная i: " + i);
+    Console.WriteLine("переменная j: " + j);
+}
